4bd428ff6ac31748aaa02d7adeada736633ab8c4
Colten Jackson <Colten Jackson>
2016-10-13 12:12:16 -0400

    Created first route

--- browser/react/app.js
+++ browser/react/app.js
@@ -1,14 +1,32 @@
 'use strict';
 
 import React from 'react';
+import Router from 'react-router'
 import ReactDOM from 'react-dom';
 import store from './store';
-import { Provider } from 'react-redux';
+import {Provider} from 'react-redux';
 import AppContainer from './containers/AppContainer';
+
+class BaseRoute extends React.Component {
+  render() {
+    return (
+      <div id="main" className="container-fluid">
+        <div className="col-xs-2">
+          <SidebarContainer/>
+        </div>
+        <div className="col-xs-10">
+          {this.props.children}
+        </div>
+        <PlayerContainer/>
+      </div>
+    )
+  }
+
+}
 
 ReactDOM.render(
   <Provider store={store}>
-    <AppContainer /> 
-  </Provider>,
-  document.getElementById('app')
-);
+  <Router history={hashHistory}>
+    <Route path='/' component={BaseRoute}></Route>
+  </Router>
+</Provider>, document.getElementById('app'));
