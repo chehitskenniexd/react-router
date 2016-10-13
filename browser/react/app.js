3a62e20e13d554371771438392975133a786c267
Kenneth Moy <moykenneth91@gmail.com>
2016-10-13 12:17:40 -0400

    successfully render sidebar and player from base route

--- browser/react/app.js
+++ browser/react/app.js
@@ -1,11 +1,13 @@
 'use strict';
 
 import React from 'react';
-import Router from 'react-router'
+import { Router, hashHistory, Route, IndexRoute } from 'react-router';
 import ReactDOM from 'react-dom';
 import store from './store';
 import {Provider} from 'react-redux';
 import AppContainer from './containers/AppContainer';
+import SidebarContainer from './containers/SidebarContainer'
+import PlayerContainer from './containers/PlayerContainer'
 
 class BaseRoute extends React.Component {
   render() {
@@ -26,7 +28,7 @@ class BaseRoute extends React.Component {
 
 ReactDOM.render(
   <Provider store={store}>
-  <Router history={Router.hashHistory}>
+  <Router history={hashHistory}>
     <Route path='/' component={BaseRoute}></Route>
   </Router>
 </Provider>, document.getElementById('app'));
