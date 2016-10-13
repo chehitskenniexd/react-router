fa3f33830f21ee7794df177ae174e8cf44226dc5
Colten Jackson <Colten Jackson>
2016-10-13 12:45:16 -0400

    Fixed rendering

--- browser/react/app.js
+++ browser/react/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import React from 'react';
-import { Router, hashHistory, Route, IndexRoute, children } from 'react-router';
+import { Router, hashHistory, Route, IndexRoute } from 'react-router';
 import ReactDOM from 'react-dom';
 import store from './store';
 import {Provider} from 'react-redux';
@@ -13,29 +13,12 @@ import ArtistsContainer from './containers/ArtistsContainer';
 
 
 
-class BaseRoute extends React.Component {
-  render() {
-    return (
-      <div id="main" className="container-fluid">
-
-        <div className="col-xs-2">
-          <SidebarContainer/>
-        </div>
-        <div className="col-xs-10">
-          {this.props.children}
-        </div>
-        <PlayerContainer/>
-      </div>
-    )
-  }
-}
-
 ReactDOM.render(
   <Provider store={store}>
   <Router history={hashHistory}>
-    <Route path='/' component={BaseRoute}>
-      <Route path='/Albums' component={AlbumsContainer}/>
-      <Route path='/Artists' component={ArtistsContainer}/>
+    <Route path='/' component={AppContainer}>
+      <Route path='/albums' component={AlbumsContainer}/>
+      <Route path='/artists' component={ArtistsContainer}/>
     </Route>
   </Router>
 </Provider>, document.getElementById('app'));
